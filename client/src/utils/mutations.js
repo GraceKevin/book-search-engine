0744e2fa5d70340ea7ca269ec2a1b2c4f63a7366
Kevin Grace <kgrace90@yahoo.com>
2021-11-14 21:37:45 -0500

    fixed .js where gql was not proper. fixed savedbooks for order of function

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,62 @@
+import gql from "graphql-tag";
+
+// Login token with ID and User
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+}`;
+
+// Add User Token with username id email
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $password: String!, $email: String!) {
+    addUser(username: $username, password: $password, email: $email) {
+      token
+      user {
+        username
+        _id
+        email
+      }
+    }
+}`;
+
+// Save Book 
+export const SAVE_BOOK = gql`
+  mutation saveBook($input: StoreBookInput) {
+    saveBook(input: $input) {
+      username
+      _id
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+}`;
+
+// Remove Book
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+}`;
